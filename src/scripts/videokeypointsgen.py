4204dd0af5f099940afdbdab041d75277e6bc1c5
Disha <dmzambani@gmail.com>
2023-03-04 13:42:23 -0800

    updates keypoint script and video keypoints

--- src/scripts/videokeypointsgen.py
+++ src/scripts/videokeypointsgen.py
@@ -2,30 +2,37 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import sys
+import os
+directory = '../dance_moves'
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose 
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) 
-print(pose) 
-cap = cv2.VideoCapture(sys.argv[1])
-if cap.isOpened() == False:
-    print("Error opening video stream or file")
-    raise TypeError
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-outdir, inputflnm = sys.argv[1][:sys.argv[1].rfind('/')+1], sys.argv[1][sys.argv[1].rfind('/')+1:]
-inflnm, inflext = inputflnm.split('.')
-out_filename = f'{outdir}{inflnm}_annotated.{inflext}'
-out = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-while cap.isOpened():
-    ret, image = cap.read()
-    if not ret:
-        break
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)         
-    image.flags.writeable = False
-    results = pose.process(image)
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)      
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+#print(pose) 
+for filename in os.listdir(directory):
+    keypoint_fname = os.path.splitext(filename)[0] + "_keypoints.txt"
+    keypoint_dname = directory + "_keypoints"
+    f = os.path.join(directory, filename)
+    k = os.path.join(keypoint_dname, keypoint_fname)
+    if os.path.exists(k):
+        os.remove(k)
+    if not os.path.isfile(f):
+        print("Error finding video stream or file")
+        raise TypeError
+    g = open(k, "x")
+    cap = cv2.VideoCapture(f)
+    if cap.isOpened() == False:
+        print("Error opening video stream or file")
+        raise TypeError
+    while cap.isOpened():
+        ret, image = cap.read()
+        if not ret:
+            break
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)         
+        image.flags.writeable = False
+        results = pose.process(image)
+        g.write(str(results.pose_landmarks))
+    g.close()
+    cap.release()
 pose.close()
-cap.release()
+
